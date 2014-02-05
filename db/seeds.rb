cc5d9690c8578ce91133240178cb26cfaf66abb0
harbin86 <harbin86@gmail.com>
2014-02-05 20:53:13 +0800

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = AdminUser.new(first_name: 'admin', last_name: 'admin', username: 'administrator', password: 'adminpassword', password_confirmation: 'adminpassword', role: 'admin')
+admin.save
